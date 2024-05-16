cdfd9cc8d46a17eb81a0a6d29339a1c02565073f
bitfroster <bitfroster@gmail.com>
2024-05-16 17:28:13 +0300

    import fixes

--- models/recipe.py
+++ models/recipe.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 from pydantic import BaseModel
 from .recipe_ingredient import RecipeIngredient
 
@@ -6,4 +6,4 @@ from .recipe_ingredient import RecipeIngredient
 class Recipe(BaseModel):
     name: str
     ingredients: List[RecipeIngredient]
-    preperations: List[str] | None
+    preperations: Optional[str] = None
